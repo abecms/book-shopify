46b0d0126b33c634a3865a089effc424c8997ecf
Greg Besson <bessong@gmail.com>
2020-03-04 13:03:56 +0100

    doc shopify

--- sidebars.js
+++ sidebars.js
@@ -8,7 +8,7 @@
 module.exports = {
   docs: {
     Setup: ['introduction', 'setup'],
-    Documentation: ['usefullinks'],
+    Development: ['theme', 'app'],
     //Dynamisation: ['nodes', 'paragraphs', 'views', 'forms', 'taxonomies', 'tokens', 'images', 'users', 'comments', 'newsletters', 'menus', 'ckeditor', 'modules', 'coding'],
     //'Dev specific': ['dev'],
   },
