8b51f476f010d837faa665c71a4d8d7ec04be212
Mateo-Castillo <60873567+Mateo-Castillo@users.noreply.github.com>
2020-08-04 18:21:38 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       type: 'category',
       label: 'Setup',
-      items: ['introduction', 'setup'],
+      items: ['introduction', 'setup', 'useful-apps'],
     },
     {
       type: 'category',
